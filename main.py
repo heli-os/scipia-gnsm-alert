9d770171688cbda7ee23637383ec07235f596517
Taeyang Jin (Theo) <theo@bolta.io>
2024-11-02 11:15:14 +0900

    feat: add telegram integration

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import requests
 from pydub import AudioSegment
 from pydub.playback import play
 
+import telegram
+import asyncio
+
 
 def check_api(url, cookies):
     headers = {
@@ -34,15 +37,29 @@ def read_config(file_path):
         return None
 
 
+async def send_telegram_message(api_key, chat_id, message):
+    bot = telegram.Bot(token=api_key)
+    await bot.send_message(chat_id=chat_id, text=message)
+
+
+def print_and_send_telegram_message(api_key, chat_id, message):
+    print(message)
+    asyncio.run(send_telegram_message(api_key, chat_id, message))
+
+
 def main():
     config = read_config("config.yml")
     cookie = config['cookie']
     check_interval = config['check_interval_seconds']  # 초 단위로 체크 주기 설정 (예: 60초)
     target_date = config['target_date']
+    telegram_api_key = config['telegram_api_key']
+    telegram_chat_id = config['telegram_chat_id']
 
     print(f"설정된 쿠키(JSESSIONID): {cookie}")
     print(f"설정된 체크 주기: {check_interval}초")
     print(f"설정된 대상 날짜: {target_date}")
+    print(f"텔레그램 api_key: {telegram_api_key}")
+    print(f"텔레그램 chat_id: {telegram_chat_id}")
 
     api_url = f"https://www.sciencecenter.go.kr/scipia/schedules/priceList?SEMESTER_CD=SM110062233270000034&COURSE_CD=CS110062233270000190&DAY={target_date}"
     cookies = {
@@ -50,7 +67,7 @@ def main():
     }
     sound_file = "alert-109578.mp3"  # 알림음 파일 경로
 
-    print("API 모니터링을 시작합니다...")
+    print_and_send_telegram_message(telegram_api_key, telegram_chat_id, "API 모니터링을 시작합니다...")
     while True:
         try:
             now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -59,16 +76,19 @@ def main():
             if data["LectureList"] and len(data["LectureList"]) > 0:
                 current_vacancy = data["LectureList"][0]["VACANCY_CNT"]
 
-                print(f"[{now}] 현재 잔여석: {current_vacancy}")
+                print_and_send_telegram_message(telegram_api_key, telegram_chat_id,
+                                                f"[{now}] 현재 잔여석: {current_vacancy}")
 
                 if current_vacancy >= 1:
-                    print(f"[{now}] 잔여석이 1 이상입니다!")
+                    print_and_send_telegram_message(telegram_api_key, telegram_chat_id,
+                                                    f"[{now}] 잔여석이 1 이상입니다!")
                     play_sound(sound_file)
             else:
-                print(f"[{now}] LectureList가 비어있거나 존재하지 않습니다.")
-
+                print_and_send_telegram_message(telegram_api_key, telegram_chat_id,
+                                                f"[{now}] LectureList가 비어있거나 존재하지 않습니다.")
         except Exception as e:
-            print(f"[{now}] 오류 발생: {e}")
+            print_and_send_telegram_message(telegram_api_key, telegram_chat_id,
+                                            f"[{now}] 오류 발생: {e}")
 
         time.sleep(check_interval)
 
